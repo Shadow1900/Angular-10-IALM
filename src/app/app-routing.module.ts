29c79c73736d5ef54c0efa1fea609c880f3567bf
Shadow1900 <118683823+Shadow1900@users.noreply.github.com>
2023-04-12 10:31:22 -0600

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AgregarMascotaComponent } from './agregar-mascota/agregar-mascota.component';
+import { ListarMascotasComponent } from './listar-mascotas/listar-mascotas.component';
 const routes: Routes = [
-  // En un momento configuramos las rutas...
+  { path: 'mascotas', component: ListarMascotasComponent },
+  { path: 'mascotas/agregar', component: AgregarMascotaComponent },
+  { path: '', redirectTo: '/mascotas', pathMatch: 'full' }, // Cuando es la raíz
+  { path: '**', redirectTo: '/mascotas' },
 ];
 
 @NgModule({
